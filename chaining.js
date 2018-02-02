5061626ba8e24863a9e8f0c81b54c42e8fb3f9d2
Dylan B <giglett21@gmail.com>
2018-02-02 10:54:11 -0800

    finnished

--- chaining.js
+++ chaining.js
@@ -1,7 +1,7 @@
 const integers = [23, 15, 6, 3, 11, 20, 18, 7, 21, 1, 29, 10, 12, 8];
 console.log("original num", integers);
 
-let newNumbers = integers.sort(function(a,b){return(b -a)}).filter(function(a){return(a > 19)}).map(function(a){return(a*1.5-1)}).reduce(function(a,b){return(a+b)});
+let newNumbers = integers.sort(function(a,b){return(b -a)}).filter(function(a){return(a < 19)}).map(function(a){return(a*1.5-1)}).reduce(function(a,b){return(a+b)});
 
 console.log("newNumbers", newNumbers);
 var postToDom = document.getElementById("postNum");
